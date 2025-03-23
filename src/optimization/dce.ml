ac1e8c4cce805e487f32e61b625664afea319162
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-23 14:32:23 -0800

    Refactor string prefix checks to use anonymous functions for clarity

--- src/optimization/dce.ml
+++ src/optimization/dce.ml
@@ -76,7 +76,7 @@ let overrides_extern_field cf c =
 	loop c cf
 
 let is_std_file dce file =
-	List.exists (ExtString.String.starts_with file) dce.std_dirs
+	List.exists (fun e -> ExtString.String.starts_with file e) dce.std_dirs
 
 let keep_metas = [Meta.Keep;Meta.Expose]
 
